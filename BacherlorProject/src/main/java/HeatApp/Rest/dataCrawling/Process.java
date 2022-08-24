c877e35ba459b75659d2983de22948ef28424513
amirali079 <aligoli079@gmail.com>
2022-08-24 02:14:10 -0700

    add rest template configs

---
+++ BacherlorProject/src/main/java/HeatApp/Rest/dataCrawling/Process.java
@@ -0,0 +1,144 @@
+package HeatApp.Rest.dataCrawling;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+@Service
+public class Process {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+   // @PostConstruct
+    public void process() throws InterruptedException {
+
+      //  readIds();
+      //  readData();
+
+    }
+
+    void readData() throws InterruptedException {
+        System.out.println("================read data=================");
+        Set<String> ids = new HashSet<>();
+        try {
+            Scanner sc = new Scanner(new File("ids9.txt"));
+
+            for (int i = 1; i <=100 ; i++)
+                ids.add(sc.nextLine());
+            sc.close();
+            System.out.println(ids.size());
+            System.out.println(ids.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Set<String> data = new HashSet<>();
+        Gson gson = new Gson();
+
+        Integer ind = 1;
+        for (String id : ids) {
+
+
+            ResponseEntity response = RestService.getData(restTemplate,id,"ebeca2bb544d48c9ba61d8f0f73425ef");
+
+            System.out.println(response.toString());
+            JsonElement element = gson.fromJson(response.getBody().toString(), JsonElement.class);
+
+            System.out.println(ind+++" ++ "+element.toString());
+                data.add(element.toString());
+
+                Thread.sleep(1100);
+        }
+
+        try {
+            Integer baseId = 801;
+            FileWriter ft ;
+            FileWriter fj ;
+
+            for (String d:data) {
+
+               fj = new FileWriter(new File("./data/json/d"+baseId+".json"));
+               ft = new FileWriter(new File("./data/txt/d"+baseId+".txt"));
+//               fj = new FileWriter(new File("./data/d"+baseId+".json"));
+//               ft = new FileWriter(new File("./data/d"+baseId+".txt"));
+               fj.write(d);
+               ft.write(d);
+               baseId++;
+               fj.flush();
+               ft.flush();
+            }
+          //  for (JsonElement )
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("_____________________end_______________________");
+    }
+
+
+    void readIds(){
+       System.out.println("===================================");
+//       Set<String> ids = new HashSet<>();
+//       Gson gson = new Gson();
+//
+//
+//       for (int i = 1; i < 12; i++) {
+//           ResponseEntity response = RestService.get(restTemplate,1000,i*100);
+//
+//           System.out.println(response.toString());
+//           JsonElement element = gson.fromJson(response.getBody().toString(), JsonElement.class);
+//           JsonArray results = (JsonArray) element.getAsJsonObject().get("results");
+//
+//           for (JsonElement object:results)
+//               ids.add(object.getAsJsonObject().get("id").getAsString());
+//           System.out.println(ids.size());
+//       }
+//
+//
+//       System.out.println(ids.toString());
+
+       try {
+           Scanner sc = new Scanner(new File("ids.txt"));
+           Set<String> readId = new HashSet<>();
+
+           for (int i = 0; i <900 ; i++)
+               readId.add(sc.nextLine());
+
+           System.out.println(readId.size()+" --- "+readId.toString());
+
+           int  index = 0;
+           int indexFile = 1;
+           FileWriter f =  new FileWriter(new File("ids"+indexFile+".txt"));
+           for (String id : readId) {
+               if (index == 100){
+                   f.flush();
+                   f.close();
+
+                   index=0;
+                   indexFile++;
+
+                   f =  new FileWriter(new File("ids"+indexFile+".txt"));
+               }
+               f.write(id+"\n");
+               index++;
+           }
+
+           System.out.println("__________end_________");
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+    }
+}
+
---
+++ BacherlorProject/src/main/java/HeatApp/Rest/dataCrawling/RestService.java
@@ -0,0 +1,31 @@
+package HeatApp.Rest.dataCrawling;
+
+import HeatApp.Rest.RestTemplateUtil;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RestService {
+
+    public static ResponseEntity<String> get(RestTemplate restTemplate, Integer number,Integer offset) {
+
+
+        String NETWORK_URI = String.format("https://api.spoonacular.com/recipes/complexSearch?apiKey=9c9d4b13164f4c0299176f4db1bd95e9&number=%d&offset=%d",number,offset);
+
+        return RestTemplateUtil.createGet(restTemplate, NETWORK_URI,"", String .class);
+    }
+
+    public static ResponseEntity<String> getData(RestTemplate restTemplate, String number,String key) {
+
+
+        String NETWORK_URI = String.format("https://api.spoonacular.com/recipes/%s/information?apiKey=%s&includeNutrition=true",number,key);
+
+        return RestTemplateUtil.createGet(restTemplate, NETWORK_URI,"", String .class);
+    }
+
+
+}
