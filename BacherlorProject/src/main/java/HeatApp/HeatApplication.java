42c587939f1416c4ae0cc88e7f9698e2d8b1d974
amirali079 <aligoli079@gmail.com>
2022-08-24 02:10:52 -0700

    Init Project

---
+++ BacherlorProject/src/main/java/HeatApp/HeatApplication.java
@@ -0,0 +1,13 @@
+package HeatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeatApplication.class, args);
+    }
+
+}
---
+++ BacherlorProject/src/test/java/HeatApp/HeatApplicationTests.java
@@ -0,0 +1,13 @@
+package HeatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
